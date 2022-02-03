a273f8b2f8b2248b293a5e3f10dd9edd30d135fa
윤지영 <yun22287@naver.com>
2022-02-03 14:05:54 +0900

    feat : React Router 기능 추가
    react-router-dom 적용

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,27 @@ import HomePage from './HomePage.jsx';
 import LoginContainer from './login/LoginContainer.jsx';
 import SignupContainer from './signup/SignupContainer.jsx';
 
+import DressRoomPage from './dressroom/DressRoomPage.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 export default function App() {
   const { location: { pathname } } = window;
   
-  const MyComponent = {
-    '/': HomePage,
-    '/signup': SignupContainer,
-    '/login': LoginContainer,
-  }[pathname];
+  // const MyComponent = {
+  //   '/': HomePage,
+  //   '/signup': SignupContainer,
+  //   '/login': LoginContainer,
+  // }[pathname];
+
 
   return (
-    <div>
-      <MyComponent />
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dressroom" element={<DressRoomPage />} />
+          <Route path="/login" element={<LoginContainer />} />
+          <Route path="/signup" element={<SignupContainer />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
