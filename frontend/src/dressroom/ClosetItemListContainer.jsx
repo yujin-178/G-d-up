b77207db7b105fb4df7f37e4f2973589723e8d6b
onys <thkim2017@gmail.com>
2022-02-04 16:27:53 +0900

    style : 16개 item 담겨있는 목록 render
    Todo: 컴포넌트로 분리 후 반복 제거

--- frontend/src/dressroom/ClosetItemListContainer.jsx
+++ frontend/src/dressroom/ClosetItemListContainer.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
 
+import { css, jsx } from "@emotion/react";
+
 export default function ClosetItemListContainer() {
+  const ItemContainer = css`
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    padding: 0.5rem;
+    background-color: #BFAEA4;
+    width: 25rem;
+    height: 25em;
+  `
+  const ClothesItem = css`
+    position: relative;
+    background-color: #FFFFFF;
+    width: 5rem;
+    height: 5rem;
+    margin: 0.5rem;
+    align-self: center;
+    justify-self: center;
+  `
+
   return (
     <div>
       <h5>목록</h5>
+      <div css={ItemContainer}>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+        <div css={ClothesItem}>
+        </div>
+      </div>
     </div>
   )
 }
