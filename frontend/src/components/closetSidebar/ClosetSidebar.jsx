88716e28e6000d02f1422fb11722760d41136e26
woori9 <wrh95222@gmail.com>
2022-02-05 23:48:40 +0900

    feat : ClosetSidebar 컴포넌트 css 추가

--- frontend/src/components/closetSidebar/ClosetSidebar.jsx
+++ frontend/src/components/closetSidebar/ClosetSidebar.jsx
@@ -32,40 +32,53 @@ function ClosetSidebar(props) {
         placeholder="태그 입력"
         onKeyPress={onKeyPress}
       />
-      <section css={toggleContainer}>
+      <div css={toggleContainer}>
         <div css={toggleBtn({ isUserItem })} onClick={toggleIsUserItem}>
           <div css={toggleBtnCircle({ isUserItem })}></div>
         </div>
         <p css={toggleTitle}>내 옷만 보기</p>
-      </section>
-      <ul css={seasonItems}> 계절
-        {filters.season.map((item, index) => (
-          <li key={index} css={ItemList}>
-            <input type="checkbox" name={item} onChange={(event) => onChangeSeason(event.target.checked, item)} />
-            <label htmlFor={item}>{item}</label>
-          </li>
-        ))}
-      </ul>
-      <ul css={colorItems}> 색깔
-        {filters.colors.map((color, index) => {
-          const isSelected = selectedColors.includes(color);
-          return (
-            <li key={index} css={ItemList}>
-              <button
-                css={colorButton({ color, isSelected })}
-                onClick={() => onChangeColor(color)}
-              >
-              </button>
+      </div>
+      <>
+        <p>🌹 season</p>
+        <ul css={seasonItems}>
+          {filters.season.map((item, index) => (
+            <li key={index} css={menuItem}>
+              <input
+                css={checkbox}
+                type="checkbox"
+                id={item}
+                onChange={(event) => onChangeSeason(event.target.checked, item)}
+              />
+              <label css={checkboxTitle} htmlFor={item}>{item}</label>
             </li>
-          )
-        })}
-      </ul>
-      <ul>
+          ))}
+        </ul>
+      </>
+      <>
+        <p>✨ color</p>
+        <ul css={colorItems}>
+          {filters.colors.map((color, index) => {
+            const isSelected = selectedColors.includes(color);
+            return (
+              <li key={index} css={menuItem}>
+                <button
+                  css={colorButton({ color, isSelected })}
+                  onClick={() => onChangeColor(color)}
+                >
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </>
+      <ul css={tagContainer}>
         {customTags.map((value, index) => {
           return (
-            <li key={index}>
-              {value}
-              <button onClick={() => deleteCustomHandler(value)}>x</button>
+            <li key={index} css={tagItem}>
+              <p css={tagTitle}>{value}</p>
+              <button
+                css={tagDeleteBtn}
+                onClick={() => deleteCustomHandler(value)}>x</button>
             </li>
           )
         })}
@@ -76,32 +89,35 @@ function ClosetSidebar(props) {
 
 const sidebarStyle = css`
   position: relative;
-  border: 1px solid blue;
-  width: 300px;
+  width: 250px;
   height: 50%;
   box-sizing: border-box;
   padding: 30px 20px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.l);
+  font-size: 20px;
+  background-color: #fff;
 `
 
 const searchInputStyle = css`
-  border: 0;
-  outline: none;
   height: 35px;
+  outline: 0;
+  border: 0;
   border-radius: 5px;
+  border-bottom: 2px solid silver;
   width: 100%;
+  font-size: 19px;
 `
 
 const seasonItems = css`
   font-size: 15px;
-  padding: 0 5px;
+  padding: 0 1px;
 `
 
-const ItemList = css`
-  text=decoration: none;
+const menuItem = css`
+  text-decoration: none;
   font-size: 15px;
   display: block;
-  font-weight: 600;
+  padding: 15px 0px;
 `
 
 const colorItems = css`
@@ -113,8 +129,8 @@ const colorItems = css`
 
 const colorButton = ({ color, isSelected }) => css`
   background-color: #${color};
-  height: 20px;
-  width: 20px;
+  height: 25px;
+  width: 25px;
   cursor: pointer;
   border-radius: 50%;
   border: 0;
@@ -127,34 +143,82 @@ const colorButton = ({ color, isSelected }) => css`
 
 const toggleContainer = css`
   display: flex;
+  padding-top: 30px;
 `
 
 const toggleTitle = css`
   margin-left: 10px;
+  margin: 0;
+  padding: 5px 10px;
 `
 
 const toggleBtn = ({ isUserItem }) => css`
-  width: 80px;
-  height: 40px;
+  width: 60px;
+  height: 30px;
   background: grey;
   border-radius: 30px;
   transition: all 300ms ease-in-out;
   ${isUserItem &&
   `
-  background: #00acee;
+    background: #00acee;
   `}
 `
 
 const toggleBtnCircle = ({ isUserItem }) => css`
-  width: 40px;
-  height: 40px;
+  width: 30px;
+  height: 30px;
   background-color: white;
   border-radius: 50%;
   transition: all 300ms ease-in-out;
   ${isUserItem &&
   `
-  margin-left: 50%;
+    margin-left: 50%;
   `}
+`
+
+const tagContainer = css`
+  width: 80%;
+  background-color: beige;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 5px;
+`
+
+const tagItem = css`
+  position: relative;
+  display: flex;
+  text-decoration: none;
+  background-color: white;
+  height: 25px;
+  padding: 4px;
+  border-radius: 18px;
+  font-size: 15px;
+  box-shadow: 2px 2px 1px rgba(0, 0, 0, 0.1);
+  margin: 5px;
+`
+
+const checkbox = css`
+  vertical-align: middle;
+  position: relative;
+  bottom: 0.5px;
+`
+
+const checkboxTitle = css`
+  display: inline-block;
+  margin-left: 10px;
+`
+
+const tagTitle = css`
+  margin: 0;
+  padding: 4px;
+  vertical-align: middle;
+`
+
+const tagDeleteBtn = css`
+  border:none;
+  background: none;
+  padding: 0px 5px 2px 0px;
+  cursor: pointer;
 `
 
 export default ClosetSidebar;
