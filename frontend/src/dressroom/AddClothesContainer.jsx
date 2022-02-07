76313b1ff58b6fac7ffd331133b03b4cbccc38b9
윤지영 <yun22287@naver.com>
2022-02-07 10:27:08 +0900

    fix : indent size 변경 & Page 이름 변경

--- frontend/src/dressroom/AddClothesContainer.jsx
+++ frontend/src/dressroom/AddClothesContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import AddClothesPage from './AddClothesPage.jsx'
+import AddClothesPage from './AddClothes.test.jsx'
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 
