b9808507e56dd2580d111ca47199d3371ca35921
woori9 <wrh95222@gmail.com>
2022-02-08 17:08:50 +0900

    test : categories 컴포넌트 테스트 코드 추가

--- frontend/src/components/categories/Categories.test.jsx
+++ frontend/src/components/categories/Categories.test.jsx
@@ -3,16 +3,35 @@
  */
 
 import React from 'react';
-import { render } from "@testing-library/react";
-import Categories from './Categories.jsx';
-
-describe('Categories', () => {
-  it('renders Categories', () => {
-
+import { render, fireEvent } from "@testing-library/react";
+import Category from './Category.jsx';
+  
+const handleClick = jest.fn();
+  
+describe('Category', () => {
+  it('renders Category', () => {
     const { getByText } = render((
-      <Categories />
-    ))
-
-    expect(getByText(/Nav/)).not.toBeNull();
+      <Category
+        item="전체"
+        isSelected={true}
+        handleClick={handleClick}
+      />
+    ));
+  
+    expect(getByText(/전체/)).not.toBeNull();
+    expect(handleClick).not.toBeCalled();
+  });
+  it('버튼의 value에 적힌 값이 인자로 전달된다.', () => {
+    const { getByText } = render((
+      <Category
+        item="전체"
+        isSelected={true}
+        handleClick={handleClick}
+      />
+    ));
+  
+    const button = getByText('전체');
+    fireEvent.click(button);
+    expect(handleClick).toBeCalledWith('전체');
   });
 });
