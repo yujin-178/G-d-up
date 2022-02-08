1af29d8c9858547c604ef816c16fd20956629654
woori9 <wrh95222@gmail.com>
2022-02-08 17:09:29 +0900

    test : category 컴포넌트 테스트 코드 추가

---
+++ frontend/src/components/category/Category.test.jsx
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render } from "@testing-library/react";
+import Categories from './Categories.jsx';
+import { categories } from '../../constants/filter.js';
+
+const handleClick = jest.fn();
+
+describe('Categories', () => {
+  it('renders Categories', () => {
+    render((
+      <Categories
+        categories={categories}
+        selectedCategory={'전체'}
+        handleClick={handleClick}
+      />
+    ));
+
+    expect(categories).toHaveLength(6);
+    expect(categories).toContain("상의");
+    expect(categories).not.toContain("자켓");
+  });
+});
