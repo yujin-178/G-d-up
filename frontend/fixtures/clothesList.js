f7b119eb09ac5b58e31b6d141f3592f2292ce994
woori9 <wrh95222@gmail.com>
2022-02-08 17:34:07 +0900

    fix : fixtures에 있는 clothes mock data 수정
    테스트 작성을 위한 mock 데이터 수정

--- frontend/fixtures/clothesList.js
+++ frontend/fixtures/clothesList.js
@@ -7,43 +7,43 @@ export const clothesList = [{
   'laundry': 'String',
   'reg_time' : '2021-01-27',
   'imageUrl': 'String',
-  },
-];
+}];
 
 export const clothesData =  [
   {
     'id': 1,
     image: 'https://image.msscdn.net/images/goods_img/20180619/803523/803523_1_500.jpg',
     category: '상의',
-    color: '검정',
+    color: 'black',
     material: '면',
     pattern: '기타',
     season: '여름',
     custom: ['여름', '데일리'],
     laundry: 'information',
+    userName: 11,
   },
   {
     'id': 2,
     'image': 'image2',
-    'category': 'top',
-    'season': ['autumn, winter'],
-    'colors': ['white'],
-    'custom': ['daily']
+    'category': '상의',
+    season: '가을',
+    color: 'white',
+    custom: ['데일리'],
   },
   {
     'id': 3,
     'image': 'image3',
-    'category': 'bottom',
-    'season': ['summer'],
-    'colors': ['black', 'grey'],
-    'custom': ['출근']
+    'category': '하의',
+    season: '여름',
+    color: 'red',
+    custom: ['출근', '데일리'],
   },
   {
     'id': 4,
     'image': 'image4',
-    'category': 'outer',
-    'season': ['winter'],
-    'colors': ['beige', 'yellow'],
-    'custom': ['daily']
+    'category': '아우터',
+    season: '겨울',
+    color: 'black',
+    custom: ['출근'],
   },
 ];
