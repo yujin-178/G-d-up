3e909b89a29aa9ef218489cff1200e565a7988e9
윤지영 <yun22287@naver.com>
2022-02-09 07:25:08 +0900

    test : 옷 추가 test 기본 틀 완성
    드디어! modal 인식 성공!!

--- frontend/src/dressroom/closet/AddClothes.test.jsx
+++ frontend/src/dressroom/closet/AddClothes.test.jsx
@@ -4,42 +4,20 @@
 
 import React from 'react';
 import { render } from "@testing-library/react";
-import AddClothesContainer from './AddClothesContainer.jsx';
+import AddClothes from './AddClothes.jsx';
 
+import { mount, shallow } from 'enzyme';
 import Modal from 'react-modal';
 
-// jest.mock('react-modal', () => {
-//   const React = require('react');
-//   const TestReactModal = require('./mockReactModal');
-//   return TestReactModal.default;
-// });
+jest.mock('react-redux');
 
-
-
-describe('AddClothesContainer', () => {
-	it('renders AddClothesContainer', () => {
-
-		const { getByText } = render((
-			<AddClothesContainer />
-		))
-		// if (process.env.NODE_ENV !== 'test') {
-		// 	ReactModal.setAppElement('#app');
-		// }
-
-		expect(getByText(/옷 추가/)).not.toBeNull();
-	});
+describe('AddClothes', () => {
+	const dispatch = jest.fn();
 
 	it('renders react-modal', () => {
-		const wrapper = render(<AddClothesContainer />);
+		const wrapper = shallow(<AddClothes />);
 		expect(wrapper.find(Modal)).toHaveLength(1);
-	});
-
-	it('opens modal when button is clicked', () => {
-		const wrapper = render(<AddClothesContainer />);
-		expect(wrapper.find(Modal).prop('IsmodalOpen')).toBe(false);
-
-		wrapper.find('button').simulate('click');
-  	expect(wrapper.find(Modal).prop('IsmodalOpen')).toBe(true);
+		expect(wrapper.find(Modal).prop('isOpen')).toBe(false);
 	});
 	
 });
