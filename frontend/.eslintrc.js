8ab5ba3e7f1d12538e3e4a0e81417ae46eeff4bb
onys <thkim2017@gmail.com>
2022-02-08 15:14:04 +0900

    style : eslintrc 설정에 설명 추가

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -22,12 +22,12 @@ module.exports = {
   ],
 
   "rules": {
-    "semi": ["error", "always"],
-    "react/prop-types": "off",
-    "indent": ["error", 2, { "StaticBlock": { "body": 1 } }],
-    "object-curly-spacing": ["error", "always"],
-    "eol-last": ["error", "always"],
-    "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0, "maxBOF": 0 }],
+    "semi": ["error", "always"], // 세미콜론
+    "react/prop-types": "off", // prop으로 오는 arg 타입 검사해서 오류 뿜는것 무시
+    "indent": ["error", 2, { "StaticBlock": { "body": 1 } }], // indent 2spaces and object indent 한 번으로
+    "object-curly-spacing": ["error", "always"], // {} 안 1space
+    "eol-last": ["error", "always"], // 파일 막줄 공백 라인
+    "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0, "maxBOF": 0 }], // 공백라인 최대 1줄로
     
     // emotion rules
     "@emotion/syntax-preference": [2, "string"],
