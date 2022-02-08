f96f0a2f7a9bdc6e2e495056a42642c684d13740
woori9 <wrh95222@gmail.com>
2022-02-08 17:22:35 +0900

    fix : category와 categories 테스트 파일이 바뀌어서 원상 복구

--- frontend/src/components/categories/Categories.test.jsx
+++ frontend/src/components/categories/Categories.test.jsx
@@ -3,35 +3,24 @@
  */
 
 import React from 'react';
-import { render, fireEvent } from "@testing-library/react";
-import Category from './Category.jsx';
-  
+import { render } from "@testing-library/react";
+import Categories from './Categories.jsx';
+import { categories } from '../../constants/filter.js';
+ 
 const handleClick = jest.fn();
-  
-describe('Category', () => {
-  it('renders Category', () => {
-    const { getByText } = render((
-      <Category
-        item="전체"
-        isSelected={true}
+ 
+describe('Categories', () => {
+  it('renders Categories', () => {
+    render((
+      <Categories
+        categories={categories}
+        selectedCategory={'전체'}
         handleClick={handleClick}
       />
     ));
-  
-    expect(getByText(/전체/)).not.toBeNull();
-    expect(handleClick).not.toBeCalled();
-  });
-  it('버튼의 value에 적힌 값이 인자로 전달된다.', () => {
-    const { getByText } = render((
-      <Category
-        item="전체"
-        isSelected={true}
-        handleClick={handleClick}
-      />
-    ));
-  
-    const button = getByText('전체');
-    fireEvent.click(button);
-    expect(handleClick).toBeCalledWith('전체');
+ 
+    expect(categories).toHaveLength(6);
+    expect(categories).toContain("상의");
+    expect(categories).not.toContain("자켓");
   });
 });
--- frontend/src/components/category/Category.test.jsx
+++ frontend/src/components/category/Category.test.jsx
@@ -3,24 +3,35 @@
  */
 
 import React from 'react';
-import { render } from "@testing-library/react";
-import Categories from './Categories.jsx';
-import { categories } from '../../constants/filter.js';
-
+import { render, fireEvent } from "@testing-library/react";
+import Category from './Category.jsx';
+   
 const handleClick = jest.fn();
-
-describe('Categories', () => {
-  it('renders Categories', () => {
-    render((
-      <Categories
-        categories={categories}
-        selectedCategory={'전체'}
+   
+describe('Category', () => {
+  it('renders Category', () => {
+    const { getByText } = render((
+      <Category
+        item="전체"
+        isSelected={true}
         handleClick={handleClick}
       />
     ));
-
-    expect(categories).toHaveLength(6);
-    expect(categories).toContain("상의");
-    expect(categories).not.toContain("자켓");
+   
+    expect(getByText(/전체/)).not.toBeNull();
+    expect(handleClick).not.toBeCalled();
+  });
+  it('버튼의 value에 적힌 값이 인자로 전달된다.', () => {
+    const { getByText } = render((
+      <Category
+        item="전체"
+        isSelected={true}
+        handleClick={handleClick}
+      />
+    ));
+   
+    const button = getByText('전체');
+    fireEvent.click(button);
+    expect(handleClick).toBeCalledWith('전체');
   });
 });
