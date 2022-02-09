dbf7f505dbdc6b1e4a096cec52077f93ac58dcd9
onys <thkim2017@gmail.com>
2022-02-09 15:00:42 +0900

    test : ClothesListItem 테스트에 prop 내려서 통과시키기

--- frontend/src/dressroom/closet/clothesList/ClothesItemListContainer.test.jsx
+++ frontend/src/dressroom/closet/clothesList/ClothesItemListContainer.test.jsx
@@ -6,16 +6,29 @@ import React from 'react';
 import { render } from "@testing-library/react";
 import ClothesItemListContainer from './ClothesItemListContainer.jsx';
 import { MemoryRouter } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { clothesData } from '../../../../fixtures/clothesList';
+
+jest.mock('react-redux');
 
 describe('ClothesItemListContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    clothesSlice: {
+      clothes: clothesData,
+      selectedClothes: clothesData[0],
+      loading: false,
+      error: null,
+    }
+  }));
+  
   it('renders ClothesItemListContainer', () => {
-    const { getByText } = render((
+    const { getByText, getAllByText } = render((
       <MemoryRouter>
         <ClothesItemListContainer />
       </MemoryRouter>
     ))
 
     expect(getByText(/목록/)).not.toBeNull();
-    expect(getByText(/image/)).not.toBeNull();
+    expect(getAllByText(/image/)).not.toBeNull();
   });
 });
