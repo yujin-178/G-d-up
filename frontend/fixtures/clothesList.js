99f34e46b26e105512a0209e7d9e7d4abbeefaf0
woori9 <wrh95222@gmail.com>
2022-02-09 10:19:10 +0900

    style : clothesList의 key object 스타일 통일

--- frontend/fixtures/clothesList.js
+++ frontend/fixtures/clothesList.js
@@ -11,7 +11,7 @@ export const clothesList = [{
 
 export const clothesData =  [
   {
-    'id': 1,
+    id: 1,
     image: 'https://image.msscdn.net/images/goods_img/20180619/803523/803523_1_500.jpg',
     category: '상의',
     color: 'black',
@@ -23,25 +23,25 @@ export const clothesData =  [
     userName: 11,
   },
   {
-    'id': 2,
-    'image': 'image2',
-    'category': '상의',
+    id: 2,
+    image: 'image2',
+    category: '상의',
     season: '가을',
     color: 'white',
     custom: ['데일리'],
   },
   {
-    'id': 3,
-    'image': 'image3',
-    'category': '하의',
+    id: 3,
+    image: 'image3',
+    category: '하의',
     season: '여름',
     color: 'red',
     custom: ['출근', '데일리'],
   },
   {
-    'id': 4,
-    'image': 'image4',
-    'category': '아우터',
+    id: 4,
+    image: 'image4',
+    category: '아우터',
     season: '겨울',
     color: 'black',
     custom: ['출근'],
