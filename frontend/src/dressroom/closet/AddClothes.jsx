7b83683c2e57ab41e478bc9b230ab64ff3262e2c
윤지영 <yun22287@naver.com>
2022-02-09 11:09:18 +0900

    feat : Modal 리팩토링

--- frontend/src/dressroom/closet/AddClothes.jsx
+++ frontend/src/dressroom/closet/AddClothes.jsx
@@ -3,84 +3,83 @@ import { css, jsx, Global } from '@emotion/react';
 import Modal from 'react-modal';
 
 if (process.env.NODE_ENV !== 'test') {
-  Modal.setAppElement('#app');
+	Modal.setAppElement('#app')
 }
 
 export default function AddClothes({ onImgChange, preview, imgInput, modalToggle, isModalOpen }) {
-
-  return (
-    <div>
-      <Global 
+	return (
+		<div>
+			<Global 
 				styles={modalClass}
 			/>
-          <Modal
-            isOpen={isModalOpen}
-            onRequestClose={() => modalToggle(false)}
-            closeTimeoutMS={500}
-          >
-            <div css={Container}>
-              <button
-                css={CloseBtn}
-                onClick={() => modalToggle(false)}>
+					<Modal
+						isOpen={isModalOpen}
+						onRequestClose={() => modalToggle(false)}
+						closeTimeoutMS={500}
+					>
+						<div css={Container}>
+							<button
+								css={CloseBtn}
+								onClick={() => modalToggle(false)}>
 								X
-              </button>
-              <div css={imgContainer}>
-                <img
-                  src={preview}
-                  css={previewImg}
-                />
-              </div>
+							</button>
+							<div css={imgContainer}>
+								<img
+									src={preview}
+									css={previewImg}
+								/>
+							</div>
 
-              <div css={btnContainer}>
-                <input
-                  css={inputTag}
-                  ref={refParam => imgInput = refParam}
-                  type="file"
-                  accept='image/*'
-                  name="file"
-                  onChange={onImgChange}
-                />
-                <button
-                  css={inputBtn}
-                  onClick={() => imgInput.click()}
-                >
+							<div css={btnContainer}>
+								<input
+									css={inputTag}
+									ref={refParam => imgInput = refParam}
+									type="file"
+									accept='image/*'
+									name="file"
+									onChange={onImgChange}
+								/>
+								<button
+									css={inputBtn}
+									onClick={() => imgInput.click()}
+								>
 									업로드
-                </button>
-              </div>
-              <div css={detailContainer}>
-                <h3>옷 정보</h3>
-                <div css={detail}>
-                  <p>카테고리 :</p>
-                  <p>색상 :</p>
-                  <p>소재 :</p>
-                  <p>패턴 : </p>
-                </div>
-                <div>
-                  <div>
-                    <p>계절</p>
-                  </div>
-                  <p>세탁</p>
-                  <p>태그</p>
-                </div>
-                <div css={submitBtnContainer}>
-                  <button
-                    css={saveBtn}
-                    onClick={() => modalToggle(false)}
-                  >
+								</button>
+							</div>
+							<div css={detailContainer}>
+								<h3>옷 정보</h3>
+								<div css={detail}>
+									<p>카테고리 :</p>
+									<p>색상 :</p>
+									<p>소재 :</p>
+									<p>패턴 : </p>
+								</div>
+								<div>
+									<div>
+										<p>계절</p>
+									</div>
+									<p>세탁</p>
+									<p>태그</p>
+								</div>
+								<div css={submitBtnContainer}>
+									<button
+										css={saveBtn}
+										onClick={() => modalToggle(false)}
+									>
 										저장
-                  </button>
-                  <button
-                    css={cancelBtn}
-                    onClick={() => modalToggle(false)}
-                  >
+									</button>
+									<button
+										css={cancelBtn}
+										onClick={() => modalToggle(false)}
+									>
 										취소
-                  </button>
-                </div>
-              </div>
-            </div>
-          </Modal>
-    </div>
-  );
+									</button>
+								</div>
+							</div>
+						</div>
+					</Modal>
+		</div>
+	)
 }
 
 const submitBtnContainer = css`
@@ -94,7 +93,7 @@ const submitBtnContainer = css`
 	font-weight: 300;
 	text-align: center;
 	transition: 0.5s;
-`;
+`
 
 const cancelBtn = css`
 	grid-column: 4;
@@ -106,7 +105,7 @@ const cancelBtn = css`
 	border-radius: 4px;
 	box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
 	cursor: pointer;
-`;
+`
 
 const saveBtn = css`
 	grid-column: 2;
@@ -118,24 +117,24 @@ const saveBtn = css`
 	border-radius: 4px;
 	box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
 	cursor: pointer;
-`;
+`
 
 const detail = css`
 	display: grid;
 	grid-template-columns: 1fr 1fr;
 	grid-template-rows: 1fr 1fr;
-`;
+`
 
 const detailContainer = css`
 	grid-row: 3;
 	grid-column: 2;
-`;
+`
 
 const Container = css`
   display: grid;
 	grid-template-columns: 1fr 1fr 1fr;
 	grid-template-rows: repeat(7, 1fr);
-`;
+`
 
 const imgContainer = css`
 	grid-column: 2;
@@ -144,7 +143,8 @@ const imgContainer = css`
 	border: 1px solid black;
 	width: 400px;
 	height: 300px;        
-`;
+
+`
 
 const previewImg = css`
 	position: relative;
@@ -156,18 +156,18 @@ const previewImg = css`
 	height: auto;
 	margin : auto;
 	top:0; bottom:0; left:0; right:0;
-`;
+`
 
 const btnContainer = css`
 	grid-column: 2;
 	margin-top: 1rem;
 	width: 400px;
 	text-align: center;
-`;
+`
 
 const inputTag = css`
 	display : none;
-`;
+`
 
 const inputBtn = css`
 	background: #6da0cf;
@@ -196,7 +196,7 @@ const inputBtn = css`
 	-webkit-appearance: none;
 	-moz-appearance: none;
 	appearance: none;
-`;
+`
 
 const CloseBtn = css`
 	background: #c99f9f;
@@ -210,4 +210,72 @@ const CloseBtn = css`
 	margin-right: 1rem;
 
 	cursor: pointer;
-`;
+`
+
+const modalClass = css`
+.ReactModal__Overlay {
+	padding: 1rem;
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	overflow-y: hidden;
+	top: 0;
+	bottom: 0;
+	right: 0;
+	left: 0;
+	background-color: rgba(0, 0, 0, 0);
+	opacity: 0;
+	transition-property: background-color, opacity;
+	transition-duration: 500ms;
+	transition-timing-function: ease-in-out;
+	outline: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.ReactModal__Overlay--after-open {
+	background-color: rgba(0, 0, 0, 0.8);
+	opacity: 1;
+}
+
+.ReactModal__Overlay--before-close {
+	background-color: rgba(0, 0, 0, 0);
+	opacity: 0;
+}
+
+.ReactModal__Content {
+	position: relative;
+	top: auto;
+	left: auto;
+	right: auto;
+	bottom: auto;
+	margin: 0 auto;
+	border: 0;
+	border-radius: 8px;
+	outline: 0;
+	display: flex;
+	max-height:calc(100vh - 210px);
+	overflow-y: auto;
+	height: 0%;
+	width: 0%;
+	background-color: transparent;
+	transition-property: background-color, width, height;
+	transition-duration: 500ms;
+	transition-timing-function: ease-in-out;
+}
+
+.ReactModal__Content--after-open {
+	width: 55%;
+	height: 80%;
+	grid-column:4;
+	background-color: #f2f2f2;
+	justify-content: center;
+}
+
+.ReactModal__Content--before-close {
+	width: 0%;
+	height: 0%;
+	background-color: transparent;
+}
+`
