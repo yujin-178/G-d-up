66dc0dbd3f8648ecbbac465af9db494f05c1712e
윤지영 <yun22287@naver.com>
2022-02-11 15:39:19 +0900

    chore : fixtures 폴더 경로 변경

---
+++ frontend/src/fixtures/clothesList.js
@@ -0,0 +1,49 @@
+export const clothesList = [{
+  'texture' : 'cotton',
+  'hashtag' : ['daily', 'etc'],
+  'season' : 'winter',
+  'main_category' : 'top',
+  'sub_category' : 't-shirt',
+  'laundry': 'String',
+  'reg_time' : '2021-01-27',
+  'imageUrl': 'String',
+}];
+
+export const clothesData =  [
+  {
+    id: 1,
+    image: 'https://image.msscdn.net/images/goods_img/20180619/803523/803523_1_500.jpg',
+    category: '상의',
+    color: 'black',
+    material: '면',
+    pattern: '기타',
+    season: '여름',
+    custom: ['여름', '데일리'],
+    laundry: 'information',
+    userName: 11,
+  },
+  {
+    id: 2,
+    image: 'image2',
+    category: '상의',
+    season: '가을',
+    color: 'white',
+    custom: ['데일리'],
+  },
+  {
+    id: 3,
+    image: 'image3',
+    category: '하의',
+    season: '여름',
+    color: 'red',
+    custom: ['출근', '데일리'],
+  },
+  {
+    id: 4,
+    image: 'image4',
+    category: '아우터',
+    season: '겨울',
+    color: 'black',
+    custom: ['출근'],
+  },
+];
