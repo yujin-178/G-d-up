acb93c78f6155d24d45c63e15837e30d18517909
이지순 <jisoon806@gmail.com>
2022-02-11 11:10:23 +0900

    refector : 이미지 명, 경로 변경, url 변경

--- backend/src/main/java/com/web/gdup/global/config/WebConfig.java
+++ backend/src/main/java/com/web/gdup/global/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-//                .addResourceLocations("file:///home/ubuntu/backend/download/");
-                .addResourceLocations("file:///C://SSAFY//download/");
+                .addResourceLocations("file:///home/ubuntu/backend/download/");
+//                .addResourceLocations("file:///C://SSAFY//download/");
     }
 }
