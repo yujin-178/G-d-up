75f5b3d9c66b12920bcb4ea5b5aa3dc8162423b4
오서하 <seoha8952@gmail.com>
2022-02-13 16:53:19 +0900

    fix : feedcontroller에서 getFeeds 수정
    로그인한 유저에게 보이는 피드들 중에서 좋아요를 눌렀는지의 여부까지도 whetherToPush 리스트 형태로 반환

--- backend/src/main/java/com/web/gdup/domain/like/service/LikeService.java
+++ backend/src/main/java/com/web/gdup/domain/like/service/LikeService.java
@@ -1,5 +1,6 @@
 package com.web.gdup.domain.like.service;
 
+import com.web.gdup.domain.feed.dto.FeedDto;
 import com.web.gdup.domain.user.dto.UserDto;
 
 import java.util.List;
@@ -8,4 +9,6 @@ public interface LikeService {
     boolean pushLike(int feedId, String userName);
     int getLikeCnt(int feedId);
     List<String> getUsers(int feedId);
+    List<String> getWhetherToPush(List<FeedDto> feedDtoList, String userName);
+    //feedlist들 중 로그인한 사용자가 해당 피드의 좋아요를 눌렀는지 여부를 반환
 }
