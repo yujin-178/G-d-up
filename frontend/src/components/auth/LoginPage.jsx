26822d5e8e0d8c866f061fb9784eac8e39345bb4
woori9 <wrh95222@gmail.com>
2022-02-15 04:11:27 +0900

    feat : 회원가입, 로그인 기능 추가
    CSS by Jisoon

--- frontend/src/components/auth/LoginPage.jsx
+++ frontend/src/components/auth/LoginPage.jsx
@@ -1,29 +1,131 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { css } from '@emotion/react';
 
-export default function LoginPage({ onChangeEmail, onChangePassword, onClickLogin }) {
+export default function LoginPage({ emailRef, passwordRef, onClickLogin }) {
   return (
-    <div>
-      <h1>로그인 페이지</h1>
-      <label htmlFor="login-email">email</label>
-      <input
-        id="login-email"
-        type="text"
-        onChange={onChangeEmail}
-      />
-      <label htmlFor="login-password">password</label>
-      <input
-        id="login-password"
-        type="text"
-        onChange={onChangePassword}
-      />
-      <button onClick={onClickLogin}>로그인</button>
-      <Link to='/'>
-        <button>
-          뒤로
-        </button>
-      </Link>
-
+    <div css={LivingRoom}>
+      <div css={Closet}>
+        <div css={ItemsGridWrapper}>
+          <br/>
+          <div css={Title}>
+            <h1>G'd up</h1>
+          </div>
+          <div css={inputGroup}>
+            <input
+              css={inputStyle}
+              ref={emailRef}
+              id="email"
+              type="text"
+              placeholder='abc@gmail.com'
+            />
+            <br/>
+            <input
+              css={inputStyle}
+              ref={passwordRef}
+              id="password"
+              type="password"
+              placeholder='비밀번호를 입력해주세요'
+            />
+            <br/>
+            <button css={LoginBtn} onClick={onClickLogin}>로그인</button>
+          </div>
+          <hr css={hrStyle}/>
+          <p>아직 회원이 아니신가요?&nbsp;
+            <Link to='/signup'>
+              회원가입
+            </Link>
+          </p>
+        </div>
+        <Link to='/'>
+          <button css={BackBtn}>
+            뒤로
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
+
+const LivingRoom = css`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url("/images/dressroombackground.jpg");
+  background-size: cover;
+  background-position: center;
+`;
+
+const Title = css`
+  text-align: center;
+  color: white;
+  font-size: 2rem;
+  margin: 0rem;
+`;
+
+const Closet = css`
+  display: flex;
+  justify-content: center;
+  width: 60%;
+  height: 60%;
+  display:inline-block;
+  margin-top: 7%;
+`;
+
+const ItemsGridWrapper = css`
+  position: relative;
+  width: 60%;
+  height: 90%;
+  margin: 2.5rem auto;
+  background-color: #BFAEA4;
+  border-radius: 0.5rem;
+  opacity: 0.9;
+  text-align: center;
+`;
+
+const inputGroup = css`
+  padding: 5px;
+`;
+
+const inputStyle = css`
+  width: 70%;
+  height: 45px;
+  font-size: 15px;
+  border: 0;
+  border-radius: 15px;
+  outline: none;
+  background-color: #F2F2F2;
+  margin: 5px;
+`;
+
+const hrStyle = css`
+  margin-top: 5%;
+  width: 70%;
+  align: center;
+  color: #E5E5E5;
+`;
+
+const LoginBtn = css`
+  width: 70%;
+  height: 45px;
+  background: #ecc194;
+	border: 0;
+	border-radius: 15px;
+  font-size: 15px;
+	box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
+	cursor: pointer;
+  margin: 5px;
+`;
+
+const BackBtn = css`
+  width: 5rem;
+  height: 3rem;
+  position: absolute;
+  left: 75.6rem;
+  top: 44.3rem;
+  background: #ecc194;
+  border: none;
+  border-radius: 4px;
+  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 10%);
+  cursor: pointer;
+`;
--- frontend/src/components/auth/SignupPage.jsx
+++ frontend/src/components/auth/SignupPage.jsx
@@ -1,30 +1,146 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { css } from '@emotion/react';
 
-export default function SignupPage({ onChangeEmail, onChangePassword, onClickSubmit }) {
+export default function SignupPage({
+  emailRef,
+  userNameRef,
+  passwordRef,
+  passwordConfirmRef,
+  onClickSubmit
+}) {
   return (
-    <div>
-      <h1>회원가입</h1>
-      <label htmlFor="signup-email">email</label>
-      <input
-        id="signup-email"
-        type="text"
-        onChange={onChangeEmail}
-      />
-      <br />
-      <label htmlFor="signup-password">password</label>
-      <input
-        id="signup-password"
-        type="text"
-        onChange={onChangePassword}
-      />
-      <br />
-      <button onClick={onClickSubmit}>가입</button>
-      <Link to='/'>
-        <button>
-          뒤로
-        </button>
-      </Link>
+    <div css={LivingRoom}>
+      <div css={Closet}>
+        <div css={ItemsGridWrapper}>
+          <br />
+          <div css={Title}>
+            <h1>회원가입</h1>
+          </div>
+          <div css={inputGroup}>
+            <input css={inputStyle}
+              ref={emailRef}
+              id="email"
+              type="text"
+              placeholder='이메일을 입력해주세요'
+            />
+            <br/>
+            <input css={inputStyle}
+              ref={userNameRef}
+              id="username"
+              type="text"
+              placeholder='이름을 입력해주세요'
+            />
+            <br/>
+            <input css={inputStyle}
+              ref={passwordRef}
+              id="username"
+              type="password"
+              placeholder='비밀번호를 입력해주세요'
+            />
+            <br/>
+            <input css={inputStyle}
+              ref={passwordConfirmRef}
+              id="confirmation"
+              type="password"
+              placeholder='비밀번호를 확인해주세요'
+            />
+          </div>
+          <button css={LoginBtn} onClick={onClickSubmit}>가입</button>
+          <hr css={hrStyle} />
+          <Link to='/login'>
+              이미 회원이신가요?
+          </Link>
+        </div>
+        <Link to='/'>
+          <button css={BackBtn}>
+            뒤로
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
+
+const LivingRoom = css`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url("/images/dressroombackground.jpg");
+  background-size: cover;
+  background-position: center;
+`;
+
+const Title = css`
+  text-align: center;
+  color: white;
+  font-size: 1.5rem;
+  margin: 0rem;
+`;
+
+const Closet = css`
+  display: flex;
+  justify-content: center;
+  width: 60%;
+  height: 65%;
+  display:inline-block;
+  margin-top: 7%;
+`;
+
+const ItemsGridWrapper = css`
+  position: relative;
+  width: 60%;
+  height: 90%;
+  margin: 2.5rem auto;
+  background-color: #BFAEA4;
+  border-radius: 0.5rem;
+  opacity: 0.9;
+  text-align: center;
+`;
+
+const inputGroup = css`
+  padding: 5px;
+`;
+
+const inputStyle = css`
+  width: 70%;
+  height: 45px;
+  font-size: 15px;
+  border: 0;
+  border-radius: 15px;
+  outline: none;
+  background-color: #F2F2F2;
+  margin: 5px;
+`;
+
+const hrStyle = css`
+  margin-top: 5%;
+  width: 70%;
+  align: center;
+  color: #E5E5E5;
+`;
+
+const LoginBtn = css`
+  width: 70%;
+  height: 45px;
+  background: #ecc194;
+	border: 0;
+	border-radius: 15px;
+  font-size: 15px;
+	box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
+	cursor: pointer;
+  margin: 5px;
+`;
+
+const BackBtn = css`
+  width: 5rem;
+  height: 3rem;
+  position: absolute;
+  left: 75.7rem;
+  top: 47rem;
+  background: #ecc194;
+  border: none;
+  border-radius: 4px;
+  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 10%);
+  cursor: pointer;
+`;
