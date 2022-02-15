31dae2fb13d2f7677923738fdd5f7c2529b348c3
오서하 <seoha8952@gmail.com>
2022-02-15 14:30:42 +0900

    feat : JWT 관련 클래스 추가

---
+++ backend/src/main/java/com/web/gdup/domain/user/service/JwtServiceImpl.java
@@ -0,0 +1,81 @@
+package com.web.gdup.domain.user.service;
+
+import com.web.gdup.domain.model.UnAuthorizedException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+    public static final Logger logger = LoggerFactory.getLogger(JwtServiceImpl.class);
+
+    private static final String SALT = "ssafySecret";
+    private static final int EXPIRE_MINUTES = 60;
+
+    @Override
+    public <T> String create(String key, T data, String subject) {
+        String jwt = Jwts.builder().setHeaderParam("typ", "JWT").setHeaderParam("regDate", System.currentTimeMillis())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * EXPIRE_MINUTES))
+                .setSubject(subject).claim(key, data).signWith(SignatureAlgorithm.HS256, this.generateKey()).compact();
+        return jwt;
+    }
+
+    private byte[] generateKey() {
+        byte[] key = null;
+        try {
+            key = SALT.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            if (logger.isInfoEnabled()) {
+                e.printStackTrace();
+            } else {
+                logger.error("Making JWT Key Error ::: {}", e.getMessage());
+            }
+        }
+        return key;
+    }
+
+    //	전달 받은 토큰이 제대로 생성된것인지 확인 하고 문제가 있다면 UnauthorizedException을 발생.
+    @Override
+    public boolean isUsable(String jwt) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(jwt);
+            return true;
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public Map<String, Object> get(String key) {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                .getRequest();
+        String jwt = request.getHeader("access-token");
+        Jws<Claims> claims = null;
+        try {
+            claims = Jwts.parser().setSigningKey(SALT.getBytes("UTF-8")).parseClaimsJws(jwt);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            throw new UnAuthorizedException();
+        }
+        Map<String, Object> value = claims.getBody();
+        logger.info("value : {}", value);
+        return value;
+    }
+
+    @Override
+    public String getUserId() {
+        return (String) this.get("user").get("userid");
+    }
+
+}
