f83689bdf2a87e36c20bed8772b9e22655ddd2c8
오서하 <seoha8952@gmail.com>
2022-02-15 10:23:27 +0900

    feat : 암호화 알고리즘 추가, api 토큰 변경
    토근 변경, 암호화 알고리즘 sha256 적용

--- backend/src/main/java/com/web/gdup/domain/user/service/UserServiceImpl.java
+++ backend/src/main/java/com/web/gdup/domain/user/service/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.web.gdup.domain.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.Optional;
 
 @Service
@@ -16,18 +17,22 @@ public class UserServiceImpl implements  UserService{
 
     @Override
     public Optional<UserEntity> login(String email, String password) throws Exception {
-        Optional<UserEntity> user = userRepository.findUserByEmailAndPassword(email, password);
+        SHA256 sha256 = new SHA256();
+        String ciphertext = sha256.encrypt(password);
+        Optional<UserEntity> user = userRepository.findUserByEmailAndPassword(email, ciphertext);
         user.orElseThrow(()-> new Exception("null"));
         return user;
     }
 
     @Override
-    public UserEntity signup(SignupRequest request){
+    public UserEntity signup(SignupRequest request) throws NoSuchAlgorithmException {
 
         Optional<UserEntity> user = userRepository.findById(request.getUserName());
         UserEntity newUser = null;
         if(!user.isPresent()){ // 해당 유저네임으로 가입된 사람들이 없기 때문에, 진행 가능
-            newUser = userRepository.save(new UserEntity(request.getUserName(), request.getPassword(), request.getEmail()));
+            SHA256 sha256 = new SHA256();
+            String ciphertext = sha256.encrypt(request.getPassword());
+            newUser = userRepository.save(new UserEntity(request.getUserName(), ciphertext, request.getEmail()));
         }
         return newUser;
     }
