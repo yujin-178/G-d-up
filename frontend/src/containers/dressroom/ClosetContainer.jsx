b9890f560f2a35d520b3901c5bbb9b2dee6670ae
woori9 <wrh95222@gmail.com>
2022-02-17 02:25:31 +0900

    fix : 옷 추가 버튼 위치 수정

--- frontend/src/containers/dressroom/ClosetContainer.jsx
+++ frontend/src/containers/dressroom/ClosetContainer.jsx
@@ -1,13 +1,8 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { filteredClothesSelector } from '../../filterSelector';
-// import { css, jsx } from '@emotion/react';
 import { resetFilter } from '../../slices/filterSlice';
 import { useNavigate } from 'react-router-dom';
-
-import {
-  changeisModalOpen
-} from '../../slices/modalSlice';
 
 import ClosetPage from '../../components/dressroom/ClosetPage.jsx';
 import { sessionLogin } from '../../slices/authSlice';
@@ -16,8 +11,6 @@ export default function ClosetContainer() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const filteredClothes = useSelector(state => filteredClothesSelector(state));
-  const loggedInUser = useSelector(state => state.authSlice.userName);
-  const { userName } = useSelector(state => state.clothesSlice);
   console.log(filteredClothes);
 
   useEffect(() => {
@@ -34,20 +27,10 @@ export default function ClosetContainer() {
     navigate('/dressroom');
   };
 
-  function handleClickModal() {
-    dispatch(changeisModalOpen(true));
-  }
-
   return (
     <ClosetPage
       filteredClothes={filteredClothes}
-      onClickModal={handleClickModal}
       goBackHandler={goBackHandler}
-      isLoggedInUser={loggedInUser === userName}
     />
   );
 }
-
-// const GridContainer = css`
-// 	display: grid;
-// `;
